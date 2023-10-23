fe31245daf2651c73326e55b8c3f20a7c26594e4
spsalmon <psalmonsacha@gmail.com>
2023-10-23 11:12:24 +0200

    added deep learning segmentation

--- pipeline_scripts/straighten.py
+++ pipeline_scripts/straighten.py
@@ -10,36 +10,46 @@ import argparse
 import yaml
 import utils
 
-def straighten_and_save(source_image_path, source_image_channels, mask_path, output_path, channel_to_allign=0):
+def straighten_and_save(source_image_path, source_image_channels, mask_path, output_path, is_zstack=False, channel_to_allign=None):
     """Straighten image and save to output_path."""
     mask = image_handling.read_tiff_file(mask_path)
-    if source_image_path is None:
-        image = mask
-    else:
-        image = image_handling.read_tiff_file(source_image_path)
-
-
-        image_to_allign = image[channel_to_allign]
-
+    image = get_image(source_image_path, mask, is_zstack, channel_to_allign, source_image_channels)
+    
     try:
-        mask = binary_image.get_biggest_object(mask)
-        warper = Warper.from_img(image, mask)
-
-        straightened_channels = []
-        for channel in source_image_channels:
-            straightened_channels.append(warper.warp_2D_img(image[channel], 0))
-
-        straightened_image = np.stack(straightened_channels, axis=0)
-
-        if np.max(straightened_image) == 1 and np.min(straightened_image) == 0:
-            straightened_image = binary_fill_holes(straightened_image)
-            straightened_image = straightened_image.astype(np.uint8)
+        if is_zstack:
+            straightened_image = straighten_zstack_image(image, mask)
         else:
-            straightened_image = straightened_image.astype(np.uint16)
+            straightened_image = straighten_2D_image(image, mask)
     except:
         straightened_image = np.zeros_like(mask).astype(np.uint8)
     
     imwrite(output_path, straightened_image, compression="zlib")
+
+def get_image(source_image_path, mask, is_zstack, channel_to_allign, source_image_channels):
+    """Get the image to be straightened."""
+    if source_image_path is None:
+        return mask
+    
+    if is_zstack:
+        full_image = image_handling.read_tiff_file(source_image_path)
+        return {
+            "allign": full_image[:, channel_to_allign, ...],
+            "straighten": full_image[:, source_image_channels, ...]
+        }
+    else:
+        return image_handling.read_tiff_file(source_image_path, channels_to_keep=source_image_channels)
+
+def straighten_zstack_image(image, mask):
+    """Straighten each plane of a zstack image."""
+    warper = Warper.from_img(image["allign"], mask)
+    straightened_channels = [warper.warp_3D_img(image["straighten"][:, channel, ...]) for channel in range(image["straighten"].shape[1])]
+    return np.stack(straightened_channels, axis=1)
+
+def straighten_2D_image(image, mask):
+    """Straighten a 2D image."""
+    warper = Warper.from_img(image, mask)
+    return warper.warp_2D_img(image)
+
 
 def main(input_pickle, output_pickle, config, n_jobs):
     config = utils.load_pickles(config)[0]
@@ -49,7 +59,9 @@ def main(input_pickle, output_pickle, config, n_jobs):
     mask_files = [f['mask_path'] for f in input_files]
     os.makedirs(os.path.dirname(output_files[0]), exist_ok=True)
 
-    Parallel(n_jobs=n_jobs)(delayed(straighten_and_save)(source_file, config['straightening_source'][1], mask_file, output_path) for source_file, mask_file, output_path in zip(source_files, mask_files, output_files))
+    is_zstack = image_handling.check_if_zstack(source_files[0])
+
+    Parallel(n_jobs=n_jobs)(delayed(straighten_and_save)(source_file, config['straightening_source'][1], mask_file, output_path, is_zstack=is_zstack) for source_file, mask_file, output_path in zip(source_files, mask_files, output_files))
 
 if __name__ == '__main__':
     args = utils.basic_get_args()
