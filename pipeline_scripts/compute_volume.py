a574bb9138dfc58c4ef39a7d1eeda3e42e23f561
spsalmon <psalmonsacha@gmail.com>
2023-11-13 15:22:11 +0100

    added compute area

--- pipeline_scripts/compute_volume.py
+++ pipeline_scripts/compute_volume.py
@@ -18,19 +18,22 @@ def compute_volume_from_file_path(straightened_mask_path, pixelsize):
     str_mask = image_handling.read_tiff_file(straightened_mask_path)
     volume = worm_features.compute_worm_volume(str_mask, pixelsize)
     length = worm_features.compute_worm_length(str_mask, pixelsize)
+    area = worm_features.compute_worm_area(str_mask, pixelsize)
 
     # replace volume and length with NaN if it is 0
     if volume == 0:
         volume = np.nan
     if length == 0:
         length = np.nan
+    if area == 0:
+        area = np.nan
 
     pattern = re.compile(r'Time(\d+)_Point(\d+)')
     match = pattern.search(straightened_mask_path)
     if match:
         time = int(match.group(1))
         point = int(match.group(2))
-        return {'Time': time, 'Point': point, 'Volume': volume, 'Length': length}
+        return {'Time': time, 'Point': point, 'Volume': volume, 'Length': length, 'Area': area}
     else:
         raise ValueError("Could not extract time and point from file name.")
 
@@ -47,7 +50,7 @@ def main(input_pickle, output_file, config, n_jobs):
 
     # rename columns to match the rest of the pipeline
     output_file_basename = os.path.basename(output_file).split('_volume.csv')[0]
-    volume_dataframe.rename(columns={'Volume': f'{output_file_basename}_volume', 'Length': f'{output_file_basename}_length'}, inplace=True)
+    volume_dataframe.rename(columns={'Volume': f'{output_file_basename}_volume', 'Length': f'{output_file_basename}_length'}, 'Area': f'{output_file_basename}_area'}, inplace=True)
     volume_dataframe.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
