a94ee03b7d72db17c89df5b58535db22dee39a0f
spsalmon <psalmonsacha@gmail.com>
2023-11-15 13:59:00 +0100

    change naming of fluo quantification output to give user more freedom

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -4,7 +4,7 @@
 #SBATCH -o ./sbatch_output/pipeline.out
 #SBATCH -e ./sbatch_output/pipeline.err
 #SBATCH -c 2
-#SBATCH -t 96:00:00
+#SBATCH -t 2:00:00
 #SBATCH --mem=4GB
 
 ~/.local/bin/micromamba run -n towbintools python3 pimp_your_pipeline.py
