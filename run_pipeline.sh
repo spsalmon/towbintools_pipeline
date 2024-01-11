292b2824a937b734d3ffdf0e228d802c58901444
spsalmon <psalmonsacha@gmail.com>
2024-01-11 12:50:32 +0100

    changed slurm script to ask for some more cpus and RAM

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,8 +3,8 @@
 #SBATCH -J pipeline
 #SBATCH -o ./sbatch_output/pipeline.out
 #SBATCH -e ./sbatch_output/pipeline.err
-#SBATCH -c 2
+#SBATCH -c 8
 #SBATCH -t 48:00:00
-#SBATCH --mem=4GB
+#SBATCH --mem=8GB
 
 ~/.local/bin/micromamba run -n towbintools python3 pimp_your_pipeline.py
