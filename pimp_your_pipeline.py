583c01b3938a5eb63a0cd417eae5128c4b093389
spsalmon <psalmonsacha@gmail.com>
2024-03-18 15:02:24 +0100

    code refactorization, cleaner pimp your pipeline script

--- pimp_your_pipeline.py
+++ pimp_your_pipeline.py
@@ -8,6 +8,8 @@ from pipeline_scripts.utils import (
     create_temp_folders,
     backup_file,
     get_experiment_time_from_filemap_parallel,
+    merge_and_save_csv,
+    rename_merge_and_save_csv,
 )
 import numpy as np
 from pipeline_scripts.run_functions import (
@@ -20,6 +22,7 @@ from pipeline_scripts.run_functions import (
     run_custom,
 )
 import argparse
+from pipeline_scripts.building_blocks import build_config_of_building_blocks
 
 
 def get_args():
@@ -73,144 +76,6 @@ if "ExperimentTime" not in experiment_filemap.columns:
 print(experiment_filemap.head())
 
 building_blocks = config["building_blocks"]
-
-
-def count_building_blocks_types(building_blocks):
-    building_block_counts = {}
-    for i, building_block in enumerate(building_blocks):
-        if building_block not in building_block_counts:
-            building_block_counts[building_block] = []
-        building_block_counts[building_block] += [i]
-    return building_block_counts
-
-
-def build_config_of_building_blocks(building_blocks, config):
-    building_block_counts = count_building_blocks_types(building_blocks)
-
-    options_map = {
-        "segmentation": [
-            "rerun_segmentation",
-            "segmentation_column",
-            "segmentation_name_suffix",
-            "segmentation_method",
-            "segmentation_channels",
-            "augment_contrast",
-            "pixelsize",
-            "sigma_canny",
-            "model_path",
-            "tiler_config",
-            "RGB",
-            "activation_layer",
-            "batch_size",
-            "ilastik_project_path",
-            "ilastik_result_channel",
-        ],
-        "straightening": [
-            "rerun_straightening",
-            "straightening_source",
-            "straightening_masks",
-        ],
-        "volume_computation": [
-            "rerun_volume_computation",
-            "volume_computation_masks",
-            "pixelsize",
-        ],
-        "classification": [
-            "rerun_classification",
-            "classification_source",
-            "classifier",
-            "pixelsize",
-        ],
-        "molt_detection": [
-            "rerun_molt_detection",
-            "molt_detection_volume",
-            "molt_detection_worm_type",
-        ],
-        "fluorescence_quantification": [
-            "rerun_fluorescence_quantification",
-            "fluorescence_quantification_source",
-            "fluorescence_quantification_masks",
-            "fluorescence_quantification_normalization",
-            "pixelsize",
-        ],
-        "custom": ["custom_script_path", "custom_script_parameters"],
-    }
-
-    blocks_config = {}
-
-    for i, building_block in enumerate(building_blocks):
-        config_copy = config.copy()
-        if building_block in options_map:
-            options = options_map[building_block]
-            # assert options
-            for option in options:
-                assert (
-                    len(config[option]) == len(building_block_counts[building_block])
-                    or len(config[option]) == 1
-                ), f"{config[option]} The number of {option} options ({len(config[option])}) does not match the number of {building_block} building blocks ({len(building_block_counts[building_block])})"
-
-            # expand single options to match the number of blocks
-            for option in options:
-                if len(config_copy[option]) == 1:
-                    config_copy[option] = config[option] * len(
-                        building_block_counts[building_block]
-                    )
-
-            # find the index of the building block
-            idx = np.argwhere(
-                np.array(building_block_counts[building_block]) == i
-            ).squeeze()
-
-            # set the options for the building block
-            block_options = {}
-            for option in options:
-                block_options[option] = config_copy[option][idx]
-
-            blocks_config[i] = block_options
-
-    return blocks_config
-
-
-blocks_config = build_config_of_building_blocks(building_blocks, config)
-
-
-def rename_merge_and_save_csv(
-    experiment_filemap,
-    report_subdir,
-    csv_file,
-    column_name_old,
-    column_name_new,
-    merge_cols=["Time", "Point"],
-):
-    dataframe = pd.read_csv(csv_file)
-    dataframe.rename(columns={column_name_old: column_name_new}, inplace=True)
-    if column_name_new in experiment_filemap.columns:
-        experiment_filemap.drop(columns=[column_name_new], inplace=True)
-    experiment_filemap = experiment_filemap.merge(dataframe, on=merge_cols, how="left")
-    experiment_filemap.to_csv(
-        os.path.join(report_subdir, "analysis_filemap.csv"), index=False
-    )
-    return experiment_filemap
-
-
-def merge_and_save_csv(
-    experiment_filemap, report_subdir, csv_file, merge_cols=["Time", "Point"]
-):
-    dataframe = pd.read_csv(csv_file)
-    new_columns = [
-        column
-        for column in dataframe.columns
-        if (column != "Time" and column != "Point")
-    ]
-    for column in new_columns:
-        if column in experiment_filemap.columns:
-            experiment_filemap.drop(columns=[column], inplace=True)
-    experiment_filemap = experiment_filemap.merge(dataframe, on=merge_cols, how="left")
-    experiment_filemap.to_csv(
-        os.path.join(report_subdir, "analysis_filemap.csv"), index=False
-    )
-    return experiment_filemap
-
 
 blocks_config = build_config_of_building_blocks(building_blocks, config)
 
