3a5f0e1c2ce1154f989046178c7f102138144381
spsalmon <psalmonsacha@gmail.com>
2024-11-01 15:31:09 +0100

    updated the install script because it didnt work anymore and added visualization tools

--- pipeline_instalation_script.sh
+++ pipeline_instalation_script.sh
@@ -11,13 +11,13 @@
 
 ~/.local/bin/micromamba activate towbintools
 
+# Install imagecodecs
+
+~/.local/bin/micromamba install -c conda-forge imagecodecs
+
 # Install all the cuda stuff
 
 micromamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-
-# Install imagecodecs
-
-~/.local/bin/micromamba install -c conda-forge imagecodecs=2024.1.1
 
 # Install ilastik
 
@@ -34,5 +34,13 @@ python -m pip install -r ~/towbintools_pipeline/requirements.txt
 # Add the environment to the jupyter notebook kernel
 
 python -m ipykernel install --user --name=towbintools
+
+# Batshit insane way to make everything work, dirtiest thing ever but it works, don't ask me why
+
+pip uninstall torchvision
+
+micromamba remove pytorch
+
+micromamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 
 mkdir -p ~/towbintools_pipeline/sbatch_output
