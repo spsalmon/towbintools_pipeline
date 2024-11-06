8e913702ec7735f6e1a603881dacf1ef65a006f4
spsalmon <psalmonsacha@gmail.com>
2024-11-06 17:26:25 +0100

    modified update script

--- update_pipeline.sh
+++ update_pipeline.sh
@@ -6,3 +6,6 @@ git reset --hard origin/main
 
 # Update the towbintools package
 ~/.local/bin/micromamba run -n towbintools pip install --upgrade towbintools
+
+# Update requirements
+~/.local/bin/micromamba run -n towbintools pip install -r requirements.txt
