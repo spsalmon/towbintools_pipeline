c3758f9015e5bd0ad600428f5ecd0d5e9749db4d
spsalmon <psalmonsacha@gmail.com>
2025-03-20 14:52:24 +0100

    modified run script to create folders if they dont exist, to prevent usuless hassle

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# if the folder sbatch_output does not exist, create it
+if [ ! -d "sbatch_output" ]; then
+    mkdir sbatch_output
+fi
+# if the folder temp_files does not exist, create it
+if [ ! -d "temp_files" ]; then
+    mkdir temp_files
+fi
+
 # Pass command line arguments to the SBATCH script
 sbatch _sbatch_pipeline.sh "$@"
