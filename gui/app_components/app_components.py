3026d29e071b60ade123a7d1aa2e42bb1d110f45
spsalmon <psalmonsacha@gmail.com>
2025-05-08 16:18:33 +0200

    lots of progress towards cleaning up the gui code, almost finished

--- gui/app_components/app_components.py
+++ gui/app_components/app_components.py
@@ -7,25 +7,7 @@ from towbintools.data_analysis import compute_series_at_time_classified
 from towbintools.foundation import image_handling
 from towbintools.foundation.worm_features import get_features_to_compute_at_molt
 
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# import plotly.graph_objs as go
-# import scipy.io as sio
-# from shiny import App
-# from shiny import reactive
-# from shiny import render
-# from shiny import ui
-# from shinywidgets import output_widget
-# from shinywidgets import render_widget
-
 FEATURES_TO_COMPUTE_AT_MOLT = get_features_to_compute_at_molt()
-
-KEY_CONVERSION_MAP = {
-    "vol": "volume",
-    "len": "length",
-    "strClass": "worm_type",
-    "ecdys": "ecdysis",
-}
 
 ECDYSIS_COLUMNS = ["HatchTime", "M1", "M2", "M3", "M4"]
 
@@ -49,42 +31,32 @@ def get_backup_path(filemap_folder, filemap_name):
 
 def open_filemap(filemap_path, open_annotated=True):
     filemap_folder = os.path.dirname(filemap_path)
-    filemap_name = os.path.basename(filemap_path)
-    filemap_name = filemap_name.split(".")[0]
+    filemap_name = os.path.basename(filemap_path).split(".")[0]
 
-    if "annotated" not in filemap_name and open_annotated:
-        filemap_save_path = f"{filemap_name}_annotated.csv"
-        filemap_save_path = os.path.join(filemap_folder, filemap_save_path)
-
-        if os.path.exists(filemap_save_path):
-            print(f"Annotated filemap already exists at {filemap_save_path}")
-            print("Opening the existing filemap instead ...")
-            filemap = pl.read_csv(filemap_save_path, infer_schema_length=10000)
-            print(filemap)
-            filemap_path = filemap_save_path
-            filemap_name = os.path.basename(filemap_path)
-            filemap_name = filemap_name.split(".")[0]
-
-            # backup the filemap
-            backup_path = get_backup_path(filemap_folder, filemap_name)
-            filemap.write_csv(backup_path)
+    # If we want to open the annotated version and it's not already annotated
+    if open_annotated and "annotated" not in filemap_name:
+        annotated_name = f"{filemap_name}_annotated.csv"
+        annotated_path = os.path.join(filemap_folder, annotated_name)
+        if os.path.exists(annotated_path):
+            print(f"Annotated filemap already exists at {annotated_path}")
+            print("Opening the existing annotated filemap instead ...")
+            filemap_path = annotated_path
+            filemap_name = os.path.basename(filemap_path).split(".")[0]
         else:
-            filemap = pl.read_csv(filemap_path, infer_schema_length=10000)
-            filemap_name = os.path.basename(filemap_path)
-            filemap_name = filemap_name.split(".")[0]
+            print(f"Annotated filemap does not exist, opening original: {filemap_path}")
 
-            # backup the filemap
-            backup_path = get_backup_path(filemap_folder, filemap_name)
-            filemap.write_csv(backup_path)
-            filemap_save_path = filemap_path
-    else:
-        # backup the filemap
-        filemap = pl.read_csv(filemap_path, infer_schema_length=10000)
-        backup_path = get_backup_path(filemap_folder, filemap_name)
-        filemap.write_csv(backup_path)
-        filemap_save_path = filemap_path
+    # Read the filemap (either original or annotated)
+    filemap = pl.read_csv(
+        filemap_path,
+        infer_schema_length=10000,
+        null_values=["np.nan", "NaN", "[nan]", ""],
+    )
 
-    return filemap, filemap_save_path
+    # Backup the filemap
+    backup_path = get_backup_path(filemap_folder, filemap_name)
+    filemap.write_csv(backup_path)
+
+    return filemap, filemap_path
 
 
 def infer_n_channels(filemap):
@@ -239,9 +211,11 @@ def get_time_and_ecdysis(filemap):
         ecdysis_of_point = ecdysis_time[i]
         ecdysis_index.append(
             [
-                float(np.where(time_of_point == ecdysis)[0][0])
-                if ecdysis in time_of_point
-                else np.nan
+                (
+                    float(np.where(time_of_point == ecdysis)[0][0])
+                    if ecdysis in time_of_point
+                    else np.nan
+                )
                 for ecdysis in ecdysis_of_point
             ]
         )
