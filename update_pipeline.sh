cdff45aa6607c4ad1878405d4fd4e68c2867e9b4
spsalmon <psalmonsacha@gmail.com>
2025-06-03 12:06:53 +0200

    finally fixed the update script

--- update_pipeline.sh
+++ update_pipeline.sh
@@ -12,4 +12,6 @@ git reset --hard origin/main
 
 # install any new package added
 ~/.local/bin/micromamba run -n towbintools pip install --upgrade -r ./requirements/requirements_pip.txt
-~/.local/bin/micromamba update -n towbintools -f ./requirements/requirements_conda.txt -y
+~/.local/bin/micromamba install -n towbintools -f ./requirements/requirements_conda.txt -y
+# update all mambda packages
+~/.local/bin/micromamba update -n towbintools --all -y
