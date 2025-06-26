a0c3fcc6dd9653eae000e6a2d78803ab341aaad5
spsalmon <psalmonsacha@gmail.com>
2025-06-26 11:08:42 +0200

    fixed problem in plotting with values at molt

--- update_pipeline.sh
+++ update_pipeline.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-# Update micromamba
-~/.local/bin/micromamba self-update
+# Check if "pipeline only" argument is provided
+PIPELINE_ONLY=false
+if [[ "$1" == "pipeline-only" || "$1" == "--pipeline-only" ]]; then
+    PIPELINE_ONLY=true
+    echo "Skipping environment updates, updating pipeline only."
+fi
 
 # Update the pipeline
 git fetch origin
 git reset --hard origin/main
 
-# Recreate the environment using the lock file
-~/.local/bin/micromamba create -n towbintools -f ./requirements/conda-lock.yaml -y
+# Skip environment updates if pipeline-only mode is enabled
+if [[ "$PIPELINE_ONLY" == false ]]; then
+    echo "Updating environment..."
 
-# Add the environment to the jupyter notebook kernel
-~/.local/bin/micromamba run -n towbintools python -m ipykernel install --user --name=towbintools
+    # Update micromamba
+    ~/.local/bin/micromamba self-update
+
+    # Recreate the environment using the lock file
+    ~/.local/bin/micromamba create -n towbintools -f ./requirements/conda-lock.yaml -y
+
+    # Add the environment to the jupyter notebook kernel
+    ~/.local/bin/micromamba run -n towbintools python -m ipykernel install --user --name=towbintools
+fi
