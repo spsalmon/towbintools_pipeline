b60f8fef14a463f85d20ec5331734ee6c11e5b3b
spsalmon <psalmonsacha@gmail.com>
2025-07-17 17:30:10 +0200

    multi class training finally works!

--- deep_learning/preprocess_masks.py
+++ deep_learning/preprocess_masks.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 
+import cv2
 import numpy as np
 from joblib import delayed
 from joblib import Parallel
@@ -16,11 +17,18 @@ def get_args():
     parser.add_argument(
         "--database_path", type=str, required=True, help="Path to the database."
     )
+    parser.add_argument(
+        "--preprocessing_type",
+        type=str,
+        required=True,
+        help="Type of preprocessing to apply.",
+    )
     return parser.parse_args()
 
 
 args = get_args()
 database_path = args.database_path
+preprocessing_type = args.preprocessing_type
 
 img_dir = "images"
 mask_dir = "masks"
@@ -50,8 +58,7 @@ os.makedirs(img_output_dir, exist_ok=True)
 def binarize_and_clean_mask(mask_file, img_file, output_dir, img_output_dir):
     mask = read_tiff_file(mask_file)
     mask = mask > 0
-    mask = remove_small_objects(mask, min_size=10)
-    mask = mask.astype(np.uint8)
+    mask = remove_small_objects(mask.astype(bool), min_size=10).astype(np.uint8)
 
     if mask.sum() == 0:
         print(f"Empty mask: {mask_file}")
@@ -67,7 +74,63 @@ def binarize_and_clean_mask(mask_file, img_file, output_dir, img_output_dir):
     )
 
 
-masks = Parallel(n_jobs=-1)(
-    delayed(binarize_and_clean_mask)(mask_file, img_file, output_dir, img_output_dir)
-    for mask_file, img_file in zip(mask_files, img_files)
-)
+def binarize_and_clean_mask_with_border(
+    mask_file, img_file, output_dir, img_output_dir, border_size=5
+):
+    mask = read_tiff_file(mask_file)
+
+    if mask.sum() == 0:
+        print(f"Empty mask: {mask_file}")
+        return
+
+    preprocessed_mask = np.zeros_like(mask, dtype=np.uint8)
+    unique_labels = np.unique(mask)
+    # remove background label (0)
+    unique_labels = unique_labels[unique_labels != 0]
+    for label in unique_labels:
+        binary_mask = (mask == label).astype(np.uint8)
+
+        if cv2.connectedComponents(binary_mask)[0] > 2:
+            continue
+
+        binary_mask = remove_small_objects(
+            binary_mask.astype(bool), min_size=30
+        ).astype(np.uint8)
+        preprocessed_mask[binary_mask > 0] = 1
+
+        kernel = cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE, (border_size, border_size)
+        )
+        dilated_binary_mask = (
+            cv2.morphologyEx(binary_mask, cv2.MORPH_DILATE, kernel) > 0
+        ).astype(np.uint8)
+        border = dilated_binary_mask - binary_mask
+        preprocessed_mask[border > 0] = 2
+
+    preprocessed_mask = preprocessed_mask.astype(np.uint8)
+    imwrite(
+        os.path.join(output_dir, os.path.basename(mask_file)),
+        preprocessed_mask,
+        compression="zlib",
+    )
+    imwrite(
+        os.path.join(img_output_dir, os.path.basename(img_file)),
+        read_tiff_file(img_file),
+        compression="zlib",
+    )
+
+
+if preprocessing_type == "binarize":
+    masks = Parallel(n_jobs=-1)(
+        delayed(binarize_and_clean_mask)(
+            mask_file, img_file, output_dir, img_output_dir
+        )
+        for mask_file, img_file in zip(mask_files, img_files)
+    )
+elif preprocessing_type == "binarize_with_border":
+    masks = Parallel(n_jobs=-1)(
+        delayed(binarize_and_clean_mask_with_border)(
+            mask_file, img_file, output_dir, img_output_dir
+        )
+        for mask_file, img_file in zip(mask_files, img_files)
+    )
--- deep_learning/preprocess_masks.sh
+++ deep_learning/preprocess_masks.sh
@@ -7,7 +7,9 @@
 #SBATCH -t 1:00:00
 #SBATCH --mem=32GB
 
-DATABASE_PATH="/mnt/towbin.data/shared/spsalmon/towbinlab_segmentation_database/squid_brightfield_dataset/body"
+DATABASE_PATH="/mnt/towbin.data/shared/spsalmon/screen_analysis_segmentation_database/adult_pharynxes/"
+PREPROCESSING_TYPE="binarize_with_border"  # Change this to the desired preprocessing type
+
 
 # Run the Python script with the specified or default configuration file
-~/.local/bin/micromamba run -n towbintools python3 preprocess_masks.py --database_path "$DATABASE_PATH"
+~/.local/bin/micromamba run -n towbintools python3 preprocess_masks.py --database_path "$DATABASE_PATH" --preprocessing_type "$PREPROCESSING_TYPE"
