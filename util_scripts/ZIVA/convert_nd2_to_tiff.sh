70c8dbc77975e6b97f4d4890b737f84768fd3c34
spsalmon <psalmonsacha@gmail.com>
2025-09-29 10:58:29 +0200

    fixed nd2 conversion

--- util_scripts/ZIVA/convert_nd2_to_tiff.sh
+++ util_scripts/ZIVA/convert_nd2_to_tiff.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J convert
 #SBATCH -o convert.out
-#SBATCH -c 64
+#SBATCH -c 32
 #SBATCH -t 12:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250904_170830_573_ZIVA_40x_397_405_yap_gfp/raw_nd2/"
-OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250904_170830_573_ZIVA_40x_397_405_yap_gfp/raw/"
+INPUT_DIR="/mnt/towbin.data/shared/spradhan/20250915_embryogenesis_316_379_374_20x_Orca/raw_nd2"
+OUTPUT_DIR="/mnt/towbin.data/shared/spradhan/20250915_embryogenesis_316_379_374_20x_Orca/raw"
 ~/.local/bin/micromamba run -n towbintools python3 convert_nd2_to_tiff.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR"
