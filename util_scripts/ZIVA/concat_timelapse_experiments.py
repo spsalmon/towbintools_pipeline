bf2aed64f054dce072e4add05678e6ef1b7ab110
spsalmon <psalmonsacha@gmail.com>
2025-10-03 13:10:42 +0200

    fixed concat script

--- util_scripts/ZIVA/concat_timelapse_experiments.py
+++ util_scripts/ZIVA/concat_timelapse_experiments.py
@@ -8,13 +8,18 @@ from joblib import Parallel
 from towbintools.foundation.file_handling import get_dir_filemap
 from tqdm import tqdm
 
-dir_list = [
-    "/mnt/towbin.data/shared/spsalmon/ZIVA_40x_397_405_yap_gfp/20250904_170830_573",
-    "/mnt/towbin.data/shared/spsalmon/ZIVA_40x_397_405_yap_gfp/20250905_165257_220",
-]
+experiment_dir = "/mnt/towbin.data/shared/spsalmon/test_concat_experiments"
 
-output_dir = "/mnt/towbin.data/shared/spsalmon/ZIVA_40x_397_405_yap_gfp/raw"
-backup_dir = "/mnt/towbin.data/shared/spsalmon/ZIVA_40x_397_405_yap_gfp/"
+dir_list = [
+    "raw",
+    "raw_part2",
+    "raw_part3",
+    "raw_part4",
+]
+dir_list = [os.path.join(experiment_dir, d) for d in dir_list]
+
+output_dir = os.path.join(experiment_dir, "raw")
+backup_dir = os.path.join(experiment_dir, "backup")
 
 os.makedirs(backup_dir, exist_ok=True)
 os.makedirs(output_dir, exist_ok=True)
@@ -37,34 +42,21 @@ def replace_time_in_filename(filename, new_time):
 
 
 def concat_timelapse_experiments(first_filemap, second_filemap):
-    # adjust time in the second filemap
-    first_filemap["NewTime"] = first_filemap["Time"]
     last_time = first_filemap["Time"].max()
-    second_filemap["NewTime"] = second_filemap["Time"]
-    second_filemap["NewTime"] += last_time + 1
+    second_filemap["Time"] += last_time + 1
 
-    # remove rows with no ImagePath
     first_filemap = first_filemap[first_filemap["ImagePath"].notna()]
     second_filemap = second_filemap[second_filemap["ImagePath"].notna()]
+    combined_filemap = pd.concat([first_filemap, second_filemap], ignore_index=True)
 
-    print(second_filemap["NewTime"].values[0], second_filemap["ImagePath"].values[0])
-
-    # change target filename to reflect new time
-    first_filemap["NewFilename"] = first_filemap.apply(
-        lambda row: os.path.basename(row["ImagePath"]), axis=1
-    )
-    second_filemap["NewFilename"] = second_filemap.apply(
+    combined_filemap.reset_index(drop=True, inplace=True)
+    combined_filemap["NewFilename"] = combined_filemap.apply(
         lambda row: os.path.basename(
-            replace_time_in_filename(row["ImagePath"], row["NewTime"])
+            replace_time_in_filename(row["ImagePath"], row["Time"])
         ),
         axis=1,
     )
-
-    combined_filemap = pd.concat([first_filemap, second_filemap], ignore_index=True)
-
-    # Reset index
-    combined_filemap.reset_index(drop=True, inplace=True)
-
+    combined_filemap.sort_values(by=["Point", "Time"], inplace=True)
     return combined_filemap
 
 
@@ -79,8 +71,7 @@ def combine_all_and_move(dir_list, output_dir):
         lambda row: os.path.join(output_dir, row["NewFilename"]), axis=1
     )
 
-    # sort based on NewTime then Point
-    combined_filemap.sort_values(by=["NewTime", "Point"], inplace=True)
+    combined_filemap.sort_values(by=["Point", "Time"], inplace=True)
 
     combined_filemap.to_csv(
         os.path.join(backup_dir, "combined_filemap.csv"), index=False
@@ -94,7 +85,6 @@ def combine_all_and_move(dir_list, output_dir):
         except Exception as e:
             return f"✗ Failed to move {os.path.basename(src)}: {str(e)}"
 
-    # move images in parallel from ImagePath to OutputPath
     Parallel(n_jobs=32, backend="threading")(
         delayed(move_file)(src, dest)
         for src, dest in tqdm(
