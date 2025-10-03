b4e0a7a8db5c5c22411e1bdcd57b99f24f5f745b
spsalmon <psalmonsacha@gmail.com>
2025-10-03 13:10:42 +0200

    fixed concat script

--- util_scripts/ZIVA/convert_nd2_to_tiff.sh
+++ util_scripts/ZIVA/convert_nd2_to_tiff.sh
@@ -5,6 +5,6 @@
 #SBATCH -t 12:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/raw_nd2"
-OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/raw"
+INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/raw_nd2_part_4"
+OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/raw_part4"
 ~/.local/bin/micromamba run -n towbintools python3 convert_nd2_to_tiff.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR"
