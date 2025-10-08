d2a08b384b2274a6b92897e2b1e7b2c04878c761
spsalmon <psalmonsacha@gmail.com>
2025-10-08 14:29:45 +0200

    removed some useless comments and stuff

--- util_scripts/ZIVA/convert_nd2_to_tiff.sh
+++ util_scripts/ZIVA/convert_nd2_to_tiff.sh
@@ -5,6 +5,6 @@
 #SBATCH -t 12:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/raw_nd2_part_4"
-OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/raw_part4"
+INPUT_DIR="/mnt/towbin.data/shared/spradhan/20250929_embryogenesis_316_0uM_500uM_P0_20x_Orca_III/raw_nd2"
+OUTPUT_DIR="/mnt/towbin.data/shared/spradhan/20250929_embryogenesis_316_0uM_500uM_P0_20x_Orca_III/raw"
 ~/.local/bin/micromamba run -n towbintools python3 convert_nd2_to_tiff.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR"
--- util_scripts/ZIVA/postprocess_autofocus.sh
+++ util_scripts/ZIVA/postprocess_autofocus.sh
@@ -5,8 +5,8 @@
 #SBATCH -t 12:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250904_170830_573_ZIVA_40x_397_405_yap_gfp/raw/"
-OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20250904_170830_573_ZIVA_40x_397_405_yap_gfp/raw_best_plane/"
-CHANNEL=1
+INPUT_DIR="/mnt/towbin.data/shared/spradhan/20250929_embryogenesis_316_0uM_500uM_P0_20x_Orca_III/raw/"
+OUTPUT_DIR="/mnt/towbin.data/shared/spradhan/20250929_embryogenesis_316_0uM_500uM_P0_20x_Orca_III/raw_best_plane/"
+CHANNEL=0
 
 ~/.local/bin/micromamba run -n towbintools python3 postprocess_autofocus.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR" --channel "$CHANNEL"
