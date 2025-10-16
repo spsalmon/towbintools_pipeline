254c4dba87d72e458347de54fe44cbff7499a7bd
spsalmon <psalmonsacha@gmail.com>
2025-10-16 17:11:12 +0200

    modified gather dataset to be a little better

--- util_scripts/ZIVA/convert_nd2_to_tiff.sh
+++ util_scripts/ZIVA/convert_nd2_to_tiff.sh
@@ -5,6 +5,6 @@
 #SBATCH -t 12:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/spradhan/20250929_embryogenesis_316_0uM_500uM_P0_20x_Orca_III/raw_nd2"
-OUTPUT_DIR="/mnt/towbin.data/shared/spradhan/20250929_embryogenesis_316_0uM_500uM_P0_20x_Orca_III/raw"
+INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20251014_150718_923_ZIVA_60x_443_additional_stardist_training_data/part2/raw_nd2"
+OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20251014_150718_923_ZIVA_60x_443_additional_stardist_training_data/part2/raw"
 ~/.local/bin/micromamba run -n towbintools python3 convert_nd2_to_tiff.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR"
