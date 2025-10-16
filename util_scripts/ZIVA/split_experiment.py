4e800cb44f9bc18db3da1979e21b700ae88308fe
spsalmon <psalmonsacha@gmail.com>
2025-10-16 17:11:12 +0200

    modified gather dataset to be a little better

--- util_scripts/ZIVA/split_experiment.py
+++ util_scripts/ZIVA/split_experiment.py
@@ -6,11 +6,11 @@ from joblib import delayed
 from joblib import Parallel
 from tqdm import tqdm
 
-experiment_dir = "/mnt/towbin.data/shared/spsalmon/20250929_161842_172_ZIVA_60x_443_stardist_training/"
+experiment_dir = "/mnt/towbin.data/shared/spsalmon/20251014_150718_923_ZIVA_60x_443_additional_stardist_training_data/part1"
 image_dir = os.path.join(experiment_dir, "raw")
 channels_to_output = {
     "DIA,DIA,GFP,mCherry": "raw",
-    "GFP,mCherry": "raw_zstack",
+    "DIA,DIA": "raw_zstack",
 }
 
 for subdir in channels_to_output.values():
