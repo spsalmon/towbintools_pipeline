b3d2916edd8001b3062b7d03c2758198344065dc
spsalmon <psalmonsacha@gmail.com>
2025-10-29 17:24:20 +0100

    good progress on gather dataset

--- util_scripts/ZIVA/convert_nd2_to_tiff.sh
+++ util_scripts/ZIVA/convert_nd2_to_tiff.sh
@@ -5,6 +5,6 @@
 #SBATCH -t 12:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20251014_150718_923_ZIVA_60x_443_additional_stardist_training_data/part2/raw_nd2"
-OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20251014_150718_923_ZIVA_60x_443_additional_stardist_training_data/part2/raw"
+INPUT_DIR="/mnt/towbin.data/shared/spsalmon/20251023_115945_091_ZIVA_60x_397_405_yap_dynamics/raw_nd2"
+OUTPUT_DIR="/mnt/towbin.data/shared/spsalmon/20251023_115945_091_ZIVA_60x_397_405_yap_dynamics/raw"
 ~/.local/bin/micromamba run -n towbintools python3 convert_nd2_to_tiff.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR"
--- util_scripts/ZIVA/split_experiment.py
+++ util_scripts/ZIVA/split_experiment.py
@@ -6,7 +6,9 @@ from joblib import delayed
 from joblib import Parallel
 from tqdm import tqdm
 
-experiment_dir = "/mnt/towbin.data/shared/spsalmon/20251014_150718_923_ZIVA_60x_443_additional_stardist_training_data/part1"
+experiment_dir = (
+    "/mnt/towbin.data/shared/spsalmon/20251023_115945_091_ZIVA_60x_397_405_yap_dynamics"
+)
 image_dir = os.path.join(experiment_dir, "raw")
 channels_to_output = {
     "DIA,DIA,GFP,mCherry": "raw",
