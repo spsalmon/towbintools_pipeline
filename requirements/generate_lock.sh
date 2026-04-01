07362b8d354ec26b25532092ac5f3c1975e90587
spsalmon <psalmonsacha@gmail.com>
2026-04-01 16:45:56 +0200

    recreated lock files

--- requirements/generate_lock.sh
+++ requirements/generate_lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm conda-lock.yml
+rm *lock.yml
 
 ~/.local/bin/micromamba run -n towbintools conda-lock -f environment.yml -p linux-64 -p win-64 --kind lock
 ~/.local/bin/micromamba run -n towbintools conda-lock render --kind env
