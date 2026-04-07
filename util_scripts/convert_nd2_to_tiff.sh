29c28b539024c7bc318c19224f06627adeac951f
spsalmon <psalmonsacha@gmail.com>
2026-04-07 13:04:21 +0200

    reorganized scripts and code

--- util_scripts/convert_nd2_to_tiff.sh
+++ util_scripts/convert_nd2_to_tiff.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 24:00:00
 #SBATCH --mem=64G
 
-INPUT_DIR="/mnt/towbin.data/shared/kstojanovski/20220629_Ti2_20x_160-182-190_pumping_25C_20220629_154238_325/raw_movies_nd2/"
-OUTPUT_DIR="/mnt/towbin.data/shared/kstojanovski/20220629_Ti2_20x_160-182-190_pumping_25C_20220629_154238_325/raw_movies/"
+INPUT_DIR="/mnt/towbin.data/shared/nschoonjans/20260312_Ziva_60X_wBT443_training-dataset/raw_nd2/"
+OUTPUT_DIR="/mnt/towbin.data/shared/nschoonjans/20260312_Ziva_60X_wBT443_training-dataset/raw/"
 NJOBS=2
 ~/.local/bin/micromamba run -n towbintools python3 convert_nd2_to_tiff.py --input-dir "$INPUT_DIR" --output-dir "$OUTPUT_DIR" --njobs "$NJOBS"
