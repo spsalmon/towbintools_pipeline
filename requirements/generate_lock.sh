ac74316fffd5ac37cb050e62c5fd258b31d3c2b3
spsalmon <psalmonsacha@gmail.com>
2026-06-05 11:00:08 +0200

    updated requirements

--- requirements/generate_lock.sh
+++ requirements/generate_lock.sh
@@ -2,5 +2,5 @@
 
 rm *lock.yml
 
-~/.local/bin/micromamba run -n towbintools conda-lock -f environment.yml -p linux-64 --kind lock
-~/.local/bin/micromamba run -n towbintools conda-lock render --kind env
+~/.local/bin/micromamba run -n towbintools conda-lock lock -f environment.yml -p linux-64 --micromamba
+~/.local/bin/micromamba run -n towbintools conda-lock render
--- update_pipeline.sh
+++ update_pipeline.sh
@@ -19,12 +19,25 @@ if [[ "$PIPELINE_ONLY" == false ]]; then
     # Update micromamba
     ~/.local/bin/micromamba self-update
 
-    # Recreate the environment using the lock file
+    # Update conda packages using the lock file.
     ~/.local/bin/micromamba run -n towbintools conda-lock install --name towbintools ./requirements/conda-lock.yml
 
     # Add the environment to the jupyter notebook kernel
     ~/.local/bin/micromamba run -n towbintools python -m ipykernel install --user --name=towbintools
 
-    # for some reason, towbintools does not get updated
-    ~/.local/bin/micromamba run -n towbintools pip install -U towbintools
+#     # Explicitly install pip packages from the rendered lock file.
+#     # conda-lock install does not reliably update pip packages in existing environments,
+#     # so we extract the pip section and run pip directly with the exact locked versions.
+#     TEMP_PIP_REQS=$(mktemp /tmp/pip_requirements_XXXXXX.txt)
+#     ~/.local/bin/micromamba run -n towbintools python3 -c "
+# import yaml
+# with open('./requirements/conda-linux-64.lock.yml') as f:
+#     data = yaml.safe_load(f)
+# for dep in data.get('dependencies', []):
+#     if isinstance(dep, dict) and 'pip' in dep:
+#         print('\n'.join(dep['pip']))
+#         break
+# " > "$TEMP_PIP_REQS"
+#     ~/.local/bin/micromamba run -n towbintools pip install --require-hashes --no-deps -r "$TEMP_PIP_REQS"
+#     rm -f "$TEMP_PIP_REQS"
 fi
